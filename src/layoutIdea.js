a0fa55d2a6d05c3b2ff2c9076284094fca2d3e3b
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-02-09 01:20:24 -0500

    delete unused files

--- src/layoutIdea.js
+++
@@ -1,4 +0,0 @@
-// Smoky transparent gif z-index 2 radius over mouse that lowers opacity on hover
-// Apocalypse Mega Mart
-// Mystery box
-// Build narrative of apocalypse and history of events through interactive popups
