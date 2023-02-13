490962893fe56f1cef44c1db8829bfaf6e6f1870
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-02-13 02:49:17 -0500

    add session storage for selected cart items

--- src/js/navTabs.js
+++ src/js/navTabs.js
@@ -14,27 +14,40 @@ function select() {
 }
 
 function loadMenu(tab = 'Food') {
-
   const currTab = tab.textContent || tab;
   const menu = document.querySelector('.menu-inventory');
 
   if (currTab === 'Food') {
     menu.innerHTML = food();
-    selectCartItems(menu);
   } else {
     menu.innerHTML = gear();
-    selectCartItems(menu);
   }
+  selectCartItems(menu);
 }
 
 function selectCartItems(menu) {
   const items = menu.querySelectorAll('.menu-item');
 
   items.forEach(item => {
+    const itemName = item.querySelector('p').textContent;
+    if (sessionStorage.getItem(itemName)) {
+      item.classList.add('selected-item');
+    }
+    
     item.addEventListener('click', () => {
       item.classList.toggle('selected-item');
+      manageSessionStorage(item);
     });
   });
+}
+
+function manageSessionStorage(item) {
+  const itemName = item.querySelector('p').textContent;
+  if (sessionStorage.getItem(itemName)) {
+    sessionStorage.removeItem(itemName);
+  } else {
+    sessionStorage.setItem(itemName, "item selected");
+  }
 }
 
 export default function navTabs() {
