71055d94911b33f3986b704200b23afd3fe3f857
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-02-14 22:59:10 -0500

    create objects for menu items and programmatic creation of markup

--- src/js/food.js
+++ src/js/food.js
@@ -1,95 +1,97 @@
-export default function food() {
+const foodItems = [
+  {
+    name: 'Box of Canned Goods',
+    price: '$50',
+    src: 'assets/images/box-of-canned-goods.png'
+  },
+  {
+    name: 'Tuna Sandwich',
+    price: '$7',
+    src: 'assets/images/tuna-sandwich.png'
+  },
+  {
+    name: 'Water Jug (5 Gal)',
+    price: '$20',
+    src: 'assets/images/water-jug-5-gallon.png'
+  },
+  {
+    name: 'Coffee Beans',
+    price: '$12',
+    src: 'assets/images/coffee-beans.png'
+  },
+  {
+    name: 'Bottle of Whiskey',
+    price: '$27',
+    src: 'assets/images/whiskey-bottle.png'
+  },
+  {
+    name: 'Red Apple',
+    price: '$2',
+    src: 'assets/images/red-apple.png'
+  },
+  {
+    name: 'Old Chinese Food',
+    price: '$9',
+    src: 'assets/images/chinese-food.png'
+  },
+  {
+    name: 'Coconut',
+    price: '$7',
+    src: 'assets/images/coconut.png'
+  },
+  {
+    name: 'Bowl of Rice',
+    price: '$4',
+    src: 'assets/images/bowl-of-rice.png'
+  },
+  {
+    name: 'Canned Mystery Food',
+    price: '$5',
+    src: 'assets/images/canned-mystery-food.png'
+  },
+  {
+    name: 'Dog Food Bites',
+    price: '$3',
+    src: 'assets/images/dog-food-bites.png'
+  }
+];
+
+function createMarkup() {
+  let htmlBuffer = '';
   
-  return `
-  <div class="menu-item">
-    <div class="description">
-      <p>Box of Canned Goods</p>
-      <p>$50</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/box-of-canned-goods.png" id="menu-image">
-  </div>
-  <div class="menu-item">
-    <img src="assets/images/tuna-sandwich.png" id="menu-image">
-    <div class="description">
-      <p>Tuna Sandwich</p>
-      <p>$7</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>Water Jug (5 Gal)</p>
-      <p>$20</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/water-jug-5-gallon.png" id="menu-image">
-  </div>
-  <div class="menu-item">
-    <img src="assets/images/coffee-beans.png" id="menu-image">
-    <div class="description">
-      <p>Coffee Beans</p>
-      <p>$12</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>Bottle of Whiskey</p>
-      <p>$30</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/whiskey-bottle.png" id="menu-image">
-  </div>
+  for (let i = 0; i < foodItems.length; i ++) {
+    const name = foodItems[i].name;
+    const price = foodItems[i].price;
+    const src = foodItems[i].src;
 
-  <div class="menu-item">
-    <img src="assets/images/red-apple.png" id="menu-image">
-    <div class="description">
-      <p>Red Apple</p>
-      <p>$2</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>Old Chinese Food</p>
-      <p>$10</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/chinese-food.png" id="menu-image">
-  </div>
-  <div class="menu-item">
-    <img src="assets/images/coconut.png" id="menu-image">
-    <div class="description">
-      <p>Coconut</p>
-      <p>$7</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>Bowl of Rice</p>
-      <p>$4</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/bowl-of-rice.png" id="menu-image">
-  </div>
+    if (i % 2 === 0) {
+      htmlBuffer += `
+        <div class="menu-item">
+        <img src="${src}" id="menu-image" loading="lazy" alt="${name}" title="${name}">
+        <div class="description">
+          <p>${name}</p>
+          <p>${price}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+      `;
+    } else {
+      htmlBuffer += `
+        <div class="menu-item">
+          <div class="description">
+            <p>${name}</p>
+            <p>${price}</p>
+            <button>Add to Cart</button>
+          </div>
+          <img src="${src}" id="menu-image" loading="lazy" alt="${name}" title="${name}">
+        </div> 
+      `;
+    }
+  }
+  
+  return htmlBuffer;
+}
 
-  <div class="menu-item">
-    <img src="assets/images/canned-mystery-food.png" id="menu-image">
-    <div class="description">
-      <p>Canned Mystery Food</p>
-      <p>$5</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>Dog Food Bites</p>
-      <p>$3</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/dog-food-bites.png" id="menu-image">
-  </div>
-  `
+export default function food() {
+  return createMarkup();
 }
--- src/js/gear.js
+++ src/js/gear.js
@@ -1,110 +1,107 @@
+const gearItems = [
+  {
+    name: 'Hunting Rifle',
+    price: '$275',
+    src: 'assets/images/hunting-rifle.png'
+  },
+  {
+    name: 'Ray Gun',
+    price: '$1800',
+    src: 'assets/images/ray-gun.png'
+  },
+  {
+    name: 'Machete',
+    price: '$45',
+    src: 'assets/images/machete.png'
+  },
+  {
+    name: 'Baseball Bat',
+    price: '$20',
+    src: 'assets/images/baseball-bat.png'
+  },
+  {
+    name: '.357 Magnum Reveolver',
+    price: '$650',
+    src: 'assets/images/revolver.png'
+  },
+  {
+    name: 'Molotov Cocktail',
+    price: '$35',
+    src: 'assets/images/molotov-cocktail.png'
+  },
+  {
+    name: 'Gas Mask',
+    price: '$150',
+    src: 'assets/images/gas-mask.png'
+  },
+  {
+    name: 'Hazmat Suit',
+    price: '$600',
+    src: 'assets/images/hazmat-suit.png'
+  },
+  {
+    name: 'Banshee',
+    price: '$4500',
+    src: 'assets/images/banshee.png'
+  },
+  {
+    name: 'Minigun',
+    price: '$1750',
+    src: 'assets/images/minigun.png'
+  },
+  {
+    name: 'Flamethrower',
+    price: '$500',
+    src: 'assets/images/flame-thrower.png'
+  },
+  {
+    name: 'Robo Dog Companion',
+    price: '$1500',
+    src: 'assets/images/robo-dog.png'
+  },
+  {
+    name: 'Fidget Spinner',
+    price: '$15',
+    src: 'assets/images/fidget-spinner.png'
+  }
+];
+
+function createMarkup() {
+  let htmlBuffer = '';
+  
+  for (let i = 0; i < gearItems.length; i ++) {
+    const name = gearItems[i].name;
+    const price = gearItems[i].price;
+    const src = gearItems[i].src;
+
+    if (i % 2 === 0) {
+      htmlBuffer += `
+        <div class="menu-item">
+        <img src="${src}" id="menu-image" loading="lazy" alt="${name}" title="${name}">
+        <div class="description">
+          <p>${name}</p>
+          <p>${price}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+      `;
+    } else {
+      htmlBuffer += `
+        <div class="menu-item">
+          <div class="description">
+            <p>${name}</p>
+            <p>${price}</p>
+            <button>Add to Cart</button>
+          </div>
+          <img src="${src}" id="menu-image" loading="lazy" alt="${name}" title="${name}">
+        </div> 
+      `;
+    }
+  }
+
+  return htmlBuffer;
+}
+
 export default function gear() {
-
-  return `
-  <div class="menu-item">
-    <div class="description">
-      <p>Hunting Rifle</p>
-      <p>$275</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/hunting-rifle.png" id="menu-image">
-  </div> 
-  <div class="menu-item">
-    <img src="assets/images/ray-gun.png" id="menu-image">
-    <div class="description">
-      <p>Ray Gun</p>
-      <p>$2000</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>Machete</p>
-      <p>$45</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/machete.png" id="menu-image">
-  </div>
-  <div class="menu-item">
-    <img src="assets/images/baseball-bat.png" id="menu-image">
-    <div class="description">
-      <p>Baseball Bat</p>
-      <p>$20</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>S&W Revolver</p>
-      <p>$650</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/revolver.png" id="menu-image">
-  </div>
-  <div class="menu-item">
-    <img src="assets/images/molotov-cocktail.png" id="menu-image">
-    <div class="description">
-      <p>Molotov Cocktail</p>
-      <p>$40</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>Gas Mask</p>
-      <p>$150</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/gas-mask.png" id="menu-image">
-  </div>
-  <div class="menu-item">
-    <img src="assets/images/hazmat-suit.png" id="menu-image">
-    <div class="description">
-      <p>Hazmat Suit</p>
-      <p>$600</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>Banshee</p>
-      <p>$5000</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/banshee.png" id="menu-image">
-  </div>
-  <div class="menu-item">
-    <img src="assets/images/minigun.png" id="menu-image">
-    <div class="description">
-      <p>Minigun</p>
-      <p>$1750</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>Flamethrower</p>
-      <p>$500</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/flame-thrower.png" id="menu-image">
-  </div>
-  <div class="menu-item">
-    <img src="assets/images/robo-dog.png" id="menu-image">
-    <div class="description">
-      <p>Robo Dog Companion</p>
-      <p>$1500</p>
-      <button>Add to Cart</button>
-    </div>
-  </div>
-  <div class="menu-item">
-    <div class="description">
-      <p>Fidget Spinner</p>
-      <p>$15</p>
-      <button>Add to Cart</button>
-    </div>
-    <img src="assets/images/fidget-spinner.png" id="menu-image">
-  </div>
-  `
-
+  return createMarkup();
 }
